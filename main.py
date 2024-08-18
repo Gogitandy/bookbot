6b7c82d713554b988a271a09044370428c017abf
Gogitandy <andrew.k.highsmith@gmail.com>
2024-08-18 12:16:44 -0400

    C: added word counter

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     book_path = "books/frakenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    count = count_words(text)
+    print(text, count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 main()
