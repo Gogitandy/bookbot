701295a464a7264e18a948158766d3f2844dc21e
Gogitandy <andrew.k.highsmith@gmail.com>
2024-08-18 14:55:32 -0400

    F: added report printer

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_count = get_character_count(text)
-    print(char_count)
+    print_report(book_path, word_count, char_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -24,5 +24,28 @@ def get_character_count(text):
             char_count[lowered] = 1
 
     return char_count
+
+def print_report(path, count, chars):
+    print(f"--- Begin report of {path} ---")
+    print(f"{count} words found in the document")
+    print("")
+    sorted_chars = sort_char_count(chars)
+    for dict in sorted_chars:
+        for char, num in dict.items():
+            if char.isalpha():
+                print(f"The '{char}' character was found {num} times")
+
+def sort_on(dict):
+    return list(dict.values())[0]
+
+def sort_char_count(chars):
+    char_list = []
+
+    for char, num in chars.items():
+        char_list.append({char: num})
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
 
 main()
