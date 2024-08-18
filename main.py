ab8900202438e9e7eb1055393284930e5d2c40be
Gogitandy <andrew.k.highsmith@gmail.com>
2024-08-18 14:01:29 -0400

    E: added char counter

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frakenstein.txt"
     text = get_book_text(book_path)
-    count = get_word_count(text)
-    print(text, count)
+    word_count = get_word_count(text)
+    char_count = get_character_count(text)
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,17 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_character_count(text):
+    char_count = {}
+
+    for char in text:
+        lowered = char.lower()
+        if lowered in char_count:
+            char_count[lowered] += 1
+        else:
+            char_count[lowered] = 1
+
+    return char_count
 
 main()
