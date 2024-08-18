5244c04fd357f9c97145fc6e0a523ed3b89b43f5
Gogitandy <andrew.k.highsmith@gmail.com>
2024-08-18 12:20:38 -0400

    D: update word count name

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def main():
     book_path = "books/frakenstein.txt"
     text = get_book_text(book_path)
-    count = count_words(text)
+    count = get_word_count(text)
     print(text, count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def count_words(text):
+def get_word_count(text):
     words = text.split()
     return len(words)
 
